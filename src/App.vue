3d621d1158d57cff62e20902a7123ac31ce87958
TontonBanton <pancla13ulas@gmail.com>
2024-11-27 10:51:16 +0800

    Resetting state, storing to ref

--- src/App.vue
+++ src/App.vue
@@ -24,23 +24,27 @@
 
     <!-- Task Loop -->
     <div v-if="filter === 'all'" class="task-list">
-      <p>Total Tasks = {{ taskStore.totalCount }} </p>
+      <p>Total Tasks = {{ totalCount }} </p>      <!-- Using Store getters stored to ref as return-->
       <div v-for="task in taskStore.tasks">
         <TaskDetails :task="task" />
       </div>
-      </div>
-      <div v-if="filter === 'favs'" class="task-list">
-        <p>Fav Tasks = {{ taskStore.favCount }} </p>
+    </div>
+
+    <div v-if="filter === 'favs'" class="task-list">
+      <p>Fav Tasks = {{ favCount }} </p>          <!-- Using Store getters stored to ref as return-->
       <div v-for="task in taskStore.favs">
         <TaskDetails :task="task" />
       </div>
     </div>
 
+    <!-- Reset Store State -->
+     <button @click="taskStore.$reset">Reset State</button>
   </main>
 </template>
 
 <script>
   import { ref } from 'vue'
+  import { storeToRefs } from 'pinia';
   import TaskDetails from './components/TaskDetails.vue';
   import TaskForm from './components/TaskForm.vue';
   import { useTaskStore } from './store/TaskStore';
@@ -52,10 +56,16 @@
       const filter = ref('all')
       const taskStore = useTaskStore()
 
+      //Storing States and Getters From Store to ref variables
+      const { tasks, isLoading, favs, favCount, totalCount} = storeToRefs(taskStore)
+
       //Run Store getTask
       taskStore.getTasks()
 
-      return { taskStore, filter }
+      return {
+        taskStore, filter,
+        tasks, isLoading, favs, favCount, totalCount    //Returning stores save to ref
+      }
     }
 
   }
